0da9cb464e928effd4e3fc7e9d467d5f49a7394b
QuantumInformatik <juanguillermoalarcon@gmail.com>
2020-02-08 22:13:46 -0500

    First Commit

---
+++ src/main/java/com/inerxiait/apirestdemo/CominerxiaitapirestdemoApplication.java
@@ -0,0 +1,13 @@
+package com.inerxiait.apirestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CominerxiaitapirestdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CominerxiaitapirestdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inerxiait/apirestdemo/CominerxiaitapirestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerxiait.apirestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CominerxiaitapirestdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
